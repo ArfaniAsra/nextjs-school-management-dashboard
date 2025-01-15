b59cfdac326bddaf023e23c02249b08d8a54f3ec
Muhammad Arfani Asra <arfaniasra@fairtech.com.sg>
2025-01-15 13:23:49 +0800

    Done with Next.js School Management Dashboard UI Design Tutorial

--- src/app/dashboard/list/events/page.tsx
+++ src/app/dashboard/list/events/page.tsx
@@ -2,79 +2,72 @@ import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { role, studentsData } from "@/lib/data";
+import { eventsData, role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
-type Student = {
+type Event = {
   id: number;
-  studentId: string;
-  name: string;
-  email?: string;
-  photo: string;
-  phone?: string;
-  grade: number;
-  class: string;
-  address: string;
+  title: string;
+  class: number;
+  date: string;
+  startTime: string;
+  endTime: string;
 };
 
 const columns = [
   {
-    header: "Info",
-    accessor: "info",
+    header: "Title",
+    accessor: "title",
   },
   {
-    header: "Student ID",
-    accessor: "studentId",
+    header: "Class Name",
+    accessor: "class",
+  },
+  {
+    header: "Date",
+    accessor: "date",
     className: "hidden md:table-cell",
   },
   {
-    header: "Grade",
-    accessor: "grade",
+    header: "Start Time",
+    accessor: "startTime",
     className: "hidden md:table-cell",
   },
   {
-    header: "Phone",
-    accessor: "phone",
-    className: "hidden lg:table-cell",
-  },
-  {
-    header: "Address",
-    accessor: "address",
-    className: "hidden lg:table-cell",
+    header: "End Time",
+    accessor: "endTime",
+    className: "hidden md:table-cell",
   },
   {
     header: "Actions",
-    accessor: "actions",
+    accessor: "action",
   },
 ];
 
-const StudentListPage = () => {
-  const renderRow = (item: Student) => (
+const EventsPage = () => {
+  const renderRow = (item: Event) => (
     <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
-      <td className="flex items-center gap-4 p-4">
-        <Image src={item.photo} alt="" width={40} height={40} className="md:hidden xl:block w-10 h-10 rounded-full object-cover" />
-        <div className="flex flex-col">
-          <h3 className="font-semibold">{item.name}</h3>
-          <p className="text-xs text-gray-500">{item.class}</p>
-        </div>
-      </td>
-      <td className="hidden md:table-cell">{item.studentId}</td>
-      <td className="hidden md:table-cell">{item.grade}</td>
-      <td className="hidden md:table-cell">{item.phone}</td>
-      <td className="hidden md:table-cell">{item.address}</td>
+      <td className="flex items-center gap-4 p-4">{item.title}</td>
+      <td>{item.class}</td>
+      <td className="hidden md:table-cell">{item.date}</td>
+      <td className="hidden md:table-cell">{item.startTime}</td>
+      <td className="hidden md:table-cell">{item.endTime}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={"/list/teachers/${item.id}"}>
+          {/* <Link href={"/list/teachers/${item.id}"}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-              <Image src="/view.png" alt="" width={16} height={16} />
+              <Image src="/update.png" alt="" width={16} height={16} />
             </button>
-          </Link>
+          </Link> */}
           {role === "admin" && (
             // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
             //   <Image src="/delete.png" alt="" width={16} height={16} />
             // </button>
-            <FormModal table="student" type="delete" id={item.id} />
+            <>
+              <FormModal table="event" type="update" data={item} />
+              <FormModal table="event" type="delete" id={item.id} />
+            </>
           )}
         </div>
       </td>
@@ -85,7 +78,7 @@ const StudentListPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Students</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Events</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -99,17 +92,17 @@ const StudentListPage = () => {
               // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               //   <Image src="/create.png" alt="" width={14} height={14} />
               // </button>
-              <FormModal table="student" type="create" />
+              <FormModal table="event" type="create" />
             )}
           </div>
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={studentsData} />
+      <Table columns={columns} renderRow={renderRow} data={eventsData} />
       {/* PAGINATION */}
       <Pagination />
     </div>
   );
 };
 
-export default StudentListPage;
+export default EventsPage;
--- src/app/dashboard/list/subjects/page.tsx
+++ src/app/dashboard/list/subjects/page.tsx
@@ -1,3 +1,4 @@
+import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
@@ -8,7 +9,7 @@ import Link from "next/link";
 type Subject = {
   id: number;
   name: string;
-  teachers: string;
+  teachers: string[];
 };
 
 const columns = [
@@ -31,18 +32,22 @@ const SubjectsPage = () => {
   const renderRow = (item: Subject) => (
     <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
       <td className="flex items-center gap-4 p-4">{item.name}</td>
-      <td className="hidden md:table-cell">{item.teachers.join(",")}</td>
+      <td className="hidden md:table-cell">{item.teachers?.join(", ")}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={"/list/teachers/${item.id}"}>
+          {/* <Link href={"/list/teachers/${item.id}"}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-              <Image src="/view.png" alt="" width={16} height={16} />
+              <Image src="/update.png" alt="" width={16} height={16} />
             </button>
-          </Link>
+          </Link> */}
           {role === "admin" && (
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-              <Image src="/delete.png" alt="" width={16} height={16} />
-            </button>
+            // <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
+            //   <Image src="/delete.png" alt="" width={16} height={16} />
+            // </button>
+            <>
+              <FormModal table="subject" type="update" data={item} />
+              <FormModal table="subject" type="delete" id={item.id} />
+            </>
           )}
         </div>
       </td>
@@ -64,9 +69,10 @@ const SubjectsPage = () => {
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
             {role === "admin" && (
-              <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
-                <Image src="/plus.png" alt="" width={14} height={14} />
-              </button>
+              // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              //   <Image src="/create.png" alt="" width={14} height={14} />
+              // </button>
+              <FormModal table="subject" type="create" />
             )}
           </div>
         </div>
